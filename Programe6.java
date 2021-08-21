adb773783fe07ab65c9b672d43f76c8c44723944
knowledgeseek <knowledgeseek21@gmil.com>
2021-08-21 23:23:33 +0530

    6th program

---
+++ Programe6.java
@@ -0,0 +1,11 @@
+package classFile;
+
+public class Programe6{
+	public static void main(String [] args){
+		// expresssion
+		 float a = 7/4*9/2;
+
+		// Output
+		System.out.println("RESULT OF THE EXPRESSION ARE: - " + a);
+	}
+}
