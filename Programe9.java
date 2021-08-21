4f0d7b61ea464c18ade6ed08c961d400be774981
knowledgeseek <knowledgeseek21@gmil.com>
2021-08-22 00:08:47 +0530

    9th program

---
+++ Programe9.java
@@ -0,0 +1,14 @@
+package classFile;
+
+public class Programe9{
+	public static void main(String[] args) {
+		// given number
+		int x = 7;	
+
+		// expresssion
+		int a = x*49/x+35/x;
+
+		// Output
+		System.out.println("THE VALUE OF A IS: - " + a);
+	}
+}
