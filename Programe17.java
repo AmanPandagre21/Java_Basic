e0bfa6283ac3a8210b00814641166224527f5dee
knowledgeseek <knowledgeseek21@gmil.com>
2021-08-23 23:16:59 +0530

    17th Program

---
+++ Programe17.java
@@ -0,0 +1,27 @@
+package classFile;
+import java.util.Scanner;
+
+public class Programe17{
+	public static void main(String[] args) {
+		// making object of scanner class
+		Scanner scan = new Scanner(System.in);
+
+		// takin user inputs
+		System.out.println("PLEASE ENTER YEAR");
+		int yr = scan.nextInt();
+
+		if(yr%4 == 0){
+			if(yr%100 == 0){
+				if(yr%400 == 0){
+					System.out.println("ENTERD YEAR IS LEAP YEAR");
+				}else{
+					System.out.println("ENTERD YEAR IS NOT A LEAP YEAR");
+				}
+			}else{
+				System.out.println("ENTERD YEAR IS LEAP YEAR");
+			}
+		}else{
+			System.out.println("ENTERD YEAR IS NOT A LEAP YEAR");
+		}
+	}
+}
