ba2c5cca795b3a683fc4cd3aa019bd0c64647459
Aman Pandagre <amanpandagre21@gmail.com>
2021-08-24 23:07:39 +0530

    Program number 22

---
+++ Loop_Control/Programe22.java
@@ -0,0 +1,12 @@
+package classFile;
+
+public class Programe22{
+	public static void main(String [] args){
+		// given
+		int num=10;
+
+		for(int i=10; i>=1; i--){
+			System.out.println(num+" X "+i+" = "+num*i);
+		}
+	}
+}
