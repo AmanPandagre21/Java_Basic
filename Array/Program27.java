ca2aa2f2230a75ce9bebef95134a4d4758a62f1f
Aman Pandagre <amanpandagre21@gmail.com>
2021-08-25 23:10:38 +0530

    Program number 27

---
+++ Array/Program27.java
@@ -0,0 +1,31 @@
+package classFile;
+import java.util.Scanner;
+
+public class Program27{
+	public static void main(String[] args) {
+		// making object of scanner class
+		Scanner scan = new Scanner(System.in);
+
+		// takin user inputs
+		System.out.println("ENTER THE INDEX OF ARRAY");
+		int n = scan.nextInt();
+
+		int [] marksArry = new int[n];
+
+		System.out.println("ENTER THE MARKS IN ARRAY- ");
+		for(int i=0; i<n; i++){	
+		int arr = scan.nextInt();	
+			marksArry[i] = arr;
+		}
+
+		int total =0;
+
+		for(int i=0; i<n; i++){
+			total += marksArry[i];
+		}
+
+		float avg = total/n;
+
+		System.out.println("AVERAGE MARKS OF STUDENT ARE:- "+ avg);
+	}
+}
