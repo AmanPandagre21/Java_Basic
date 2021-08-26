6b50a0f720fbe60eb0fd7cb56ab9deb0185c53a5
Aman Pandagre <amanpandagre21@gmail.com>
2021-08-26 22:45:55 +0530

    Program number 34

---
+++ Methods/Program34.java
@@ -0,0 +1,17 @@
+package classFile;
+
+public class Program34{
+
+	public static void pattern(){
+		for(int i=0; i<6; i++){
+			for(int j=0; j<i; j++){
+				System.out.print("*");
+			}
+			System.out.println(" ");
+		}
+	}
+
+	public static void main(String[] args) {
+		pattern();	
+	}
+}
